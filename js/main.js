a02924acd5f7806b794ceb520b8a3245e78231eb
dinarcoins <nhanhoang863@gmail.com>
2021-12-01 11:08:42 +0700

    section favourite and 2 more

---
+++ js/main.js
@@ -0,0 +1,11 @@
+var swiper = new Swiper(".mySwiper", {
+	pagination: {
+		el: ".swiper-pagination",
+	},
+});
+var swiper = new Swiper(".mySwiper_booking", {
+	scrollbar: {
+		el: ".swiper-scrollbar",
+		hide: true,
+	},
+});
